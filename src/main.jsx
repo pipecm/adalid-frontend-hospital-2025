ddce3060e4c2fba18eeba4f11d39fb405bec61a1
Felipe <pipecm@mbp-pipecm.local>
2025-03-26 21:52:41 -0300

    Implementación y optimización de PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,14 @@ import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css';
 
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker.register('/sw.js')
+		.then(() => console.log("Service Worker registered successfully!"))
+		.catch(error => console.log("Service Worker registration failed: ", error));
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    headers: {
-      "X-Frame-Options": "DENY", 
-      "Content-Security-Policy": "frame-ancestors 'none';", 
-    },
-  }
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: "Hospital San Itario",
+    			short_name: "Hospital",
+				description: "App oficial del Hospital San Itario",
+				theme_color: '#ffffff',
+				start_url: '/',
+				display: 'standalone',
+				icons: [
+					{
+						src: '/icons/icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			},
+			devOptions: {
+				enabled: true
+			}
+		}
+	)],
+	server: {
+		headers: {
+			"X-Frame-Options": "DENY", 
+			"Content-Security-Policy": "frame-ancestors 'none';", 
+		},
+	}
+});
