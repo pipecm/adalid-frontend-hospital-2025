2d708e15933cf3ca42acf2244e09c93476a4980e
Felipe <pipecm@mbp-pipecm.local>
2025-04-12 21:37:47 -0400

    Optimizacion para dispositivos moviles

--- src/layouts/AppMainLayout.jsx
+++ src/layouts/AppMainLayout.jsx
@@ -8,47 +8,43 @@ const AppMainLayout = ({ children }) => {
     return (
         <div className="container">
             <header>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-primary hospital-menu">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#home">⚕️ Hospital San Itario</a>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                {user && 
-                                    <li className="nav-item nav-link">
-                                        <Link to="/">
-                                            <span className="nav-text">Inicio</span>
-                                        </Link>
-                                    </li>
-                                }
-                                {user && 
-                                    <li className="nav-item nav-link">
-                                        <Link to="/staff">
-                                            <span className="nav-text">Médicos</span>
-                                        </Link>
-                                    </li>
-                                }
-                                {user && (user['role'] === "doctor") &&
-                                    <li className="nav-item nav-link">
-                                        <Link to="/doctor">
-                                            <span className="nav-text">Perfil</span>
-                                        </Link>
-                                    </li>
-                                }
-                                {user && (user['role'] === "admin") &&
-                                    <li className="nav-item nav-link">
-                                        <Link to="/dashboard">
-                                            <span className="nav-text">Dashboard</span>
-                                        </Link>
-                                    </li>
-                                }
-                            </ul>
+                <nav className="navbar navbar-dark bg-primary hospital-menu">
+                    <div className="navbar-upper">
+                        <div className="container-login">
+                            <a className="hospital-banner navbar-brand" href="#home">⚕️ Hospital San Itario</a>
+                        </div>
+                        <div>
+                            {!user && <button onClick={() => navigate("/patient-signup")} className="btn-menu">Registro</button>}
+                            {!user && <button onClick={() => navigate("/login")} className="btn-menu">Login</button>}
+                            {user && (user['role'] === "patient") && <button onClick={() => navigate("/appointments")} className="btn-menu">Agendar cita</button>}
+                            {user && <button onClick={logout} className="btn-menu">Cerrar Sesión</button>}
                         </div>
                     </div>
-                    {!user && <button onClick={() => navigate("/patient-signup")} className="btn btn-primary btn-menu">Registro</button>}
-                    {!user && <button onClick={() => navigate("/login")} className="btn btn-primary btn-menu">Login</button>}
-                    {user && (user['role'] === "patient") && <button onClick={() => navigate("/appointments")} className="btn btn-primary btn-menu">Agendar cita</button>}
-                    {user && <button onClick={logout} className="btn btn-primary btn-menu">Cerrar Sesión</button>}
                 </nav>
+                <nav className="navbar-lower">
+                    <ul className="navbar-hospital">
+                        {user &&
+                            <li className="nav-item nav-link">
+                                <Link className="nav-text" to="/">Inicio</Link>
+                            </li>
+                        }
+                        {user && 
+                            <li className="nav-item nav-link">
+                                <Link className="nav-text" to="/staff">Médicos</Link>
+                            </li>
+                        }
+                        {user && (user['role'] === "doctor") &&
+                            <li className="nav-item nav-link">
+                                <Link className="nav-text" to="/doctor">Perfil</Link>
+                            </li>
+                        }
+                        {user && (user['role'] === "admin") &&
+                            <li className="nav-item nav-link">
+                                <Link className="nav-text" to="/dashboard">Dashboard</Link>
+                            </li>
+                        }
+                    </ul>
+                </nav>   
             </header>
             <main>{children}</main>
             <footer>
